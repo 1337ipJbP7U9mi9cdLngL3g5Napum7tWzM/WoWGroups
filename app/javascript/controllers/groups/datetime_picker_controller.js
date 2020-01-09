fd909c2733b1174d7c760b3e9e05346b4a701b8e
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-01-08 22:09:50 -0600

    groups new js finish

--- app/javascript/controllers/groups/datetime_picker_controller.js
+++ app/javascript/controllers/groups/datetime_picker_controller.js
@@ -1,17 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "input" ]
+  static targets = [ "input", "picker", "checkbox" ]
 
   connect() {
 
   }
 
   update() {
-    if (typeof(this.element.datetime) === 'undefined') {
-      this.element.now()
+    if (typeof(this.pickerTarget.datetime) === 'undefined') {
+      this.pickerTarget.now()
     }
-    this.inputTarget.value = this.element.datetime
+    this.inputTarget.value = this.pickerTarget.datetime
+  }
+
+  datepickerHidden(event) {
+    if(this.checkboxTarget.checked) {
+      this.pickerTarget.classList.remove('is-hidden')
+    } else {
+      this.pickerTarget.classList.add('is-hidden')
+    }
   }
 
 }
