d1c3b1e90e0e8312a068db0e1a3aa223ed4d5922
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-01-30 12:28:40 -0600

    removed sprockets

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,10 +33,12 @@ set :puma_init_active_record, true  # Change to true if using ActiveRecord
 set :keep_releases, 2
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{.bundle}
+set :linked_dirs,  %w{bin tmp/pids tmp/cache tmp/sockets}
+# set :linked_dirs, %w{.bundle}
+
+Rake::Task["deploy:assets:backup_manifest"].clear_actions
 
 
 namespace :puma do
