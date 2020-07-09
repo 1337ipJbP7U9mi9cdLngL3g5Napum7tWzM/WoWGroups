529c99b18c62733bdac267952fe671ec85c0a30c
DooM <1337ipJbP7U9mi9cdLngL3g5Napum7tWzM@protonmail.com>
2020-07-09 12:29:55 -0500

    home index page test group setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# t.belongs_to :user
+# t.string :group_type
+# t.boolean :private, null: false, default: false
+# t.string :private_url
+# t.integer :levels, array: true
+# t.integer :group_size
+# t.integer :group_tanks
+# t.integer :group_heals
+# t.integer :group_dps
+# t.integer :group_accepted, array: true, default: []
+# t.integer :group_declined, array: true, default: []
+# t.integer :group_reserves, array: true, default: []
+# t.belongs_to :server
+# t.integer :faction
+# t.boolean :faction_cross, null: false, default: false
+# t.string :short_description
+# t.boolean :active, null: false, default: false
+# t.datetime :time
